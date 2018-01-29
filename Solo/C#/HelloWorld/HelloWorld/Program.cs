49e6a3d34fb367bb71e320c2d66277e4ea85f276
Jacob Patrick <fivestaroblivion@gmail.com>
2018-01-29 11:20:47 -0600

    Add City, State, and Zip
    added code to print authors city, state and, zip

--- Solo/C#/HelloWorld/HelloWorld/Program.cs
+++ Solo/C#/HelloWorld/HelloWorld/Program.cs
@@ -13,6 +13,7 @@ namespace HelloWorld
 			Console.WriteLine("Hello World");
             Console.WriteLine("My name is Jacob");
             Console.WriteLine("My address is: 820 Mary Rose Ct.");
+            Console.WriteLine("Brookfield, WI, 53045");
 			Console.ReadKey();
 		}
 	}
